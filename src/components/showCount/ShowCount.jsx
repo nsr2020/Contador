8ac6cbc5110c16c7dcbe3be71b3688d2e9a76a3d
nsr2020 <146729141+nsr2020@users.noreply.github.com>
2024-03-01 15:13:21 +0100

    change

--- src/components/showCount/ShowCount.jsx
+++ src/components/showCount/ShowCount.jsx
@@ -4,7 +4,7 @@ import "./ShowCount.css"
 const ShowCount = ({clicks}) => {
     const colorClass = clicks < 0 ? 'rojo' : clicks > 0 ? 'verde' : '';
   return (
-    <p className={"frase"}>Has hecho click <span className={`${colorClass}`}>{clicks}</span> veces.</p>
+    <p className={"frase"}>Has hecho click <span className={`${colorClass}`}>{clicks} </span>{clicks === 1?"vez":"veces"}</p>
   )
 }
 
